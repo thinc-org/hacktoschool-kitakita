30fcd758ad5ddf26ce55c57e80c481ddb7d033de
Idhibhat Pankam <marginpankam@gmail.com>
2023-01-10 12:13:23 +0700

    refactor: use react-bootstrap-icons instead of bootstrap-icons

--- apps/client/src/modules/navbar/components/Navbar.tsx
+++ apps/client/src/modules/navbar/components/Navbar.tsx
@@ -1,6 +1,5 @@
 import { FC, useState } from "react";
-
-import "bootstrap-icons/font/bootstrap-icons.css";
+import { ChevronDown, FilterLeft, XLg } from "react-bootstrap-icons";
 
 import { Overlay } from "$modules/navbar/components/Overlay";
 import { ProfilePic } from "$modules/navbar/components/ProfilePic";
@@ -17,15 +16,15 @@ export const Navbar: FC = () => {
     <>
       <div className="flex h-12 w-screen items-center justify-between border-b bg-gray-light py-2 px-6 lg:py-8 lg:px-64">
         {navbarOpen ? (
-          <i
-            className="bi bi-x-lg text-2xl hover:cursor-pointer lg:hidden"
+          <XLg
+            className="text-2xl hover:cursor-pointer lg:hidden"
             role="presentation"
             onClick={() => setNavbarOpen(!navbarOpen)}
             onKeyDown={() => setNavbarOpen(!navbarOpen)}
           />
         ) : (
-          <i
-            className="bi bi-filter-left text-2xl hover:cursor-pointer lg:hidden"
+          <FilterLeft
+            className="text-2xl hover:cursor-pointer lg:hidden"
             role="presentation"
             onClick={() => setNavbarOpen(!navbarOpen)}
             onKeyDown={() => setNavbarOpen(!navbarOpen)}
@@ -51,7 +50,7 @@ export const Navbar: FC = () => {
               onKeyDown={() => setGamesOpen(!gamesOpen)}
             >
               Games
-              <i className="bi bi-chevron-down ml-1 hidden text-[10px] font-bold text-gray-dark hover:text-black lg:block" />
+              <ChevronDown className="ml-1 hidden text-[10px] font-bold text-gray-dark hover:text-black lg:block" />
               {gamesOpen && (
                 <HoverCard style={{ top: 32 }}>
                   <CardItem link="sprint" text="Sprint &#10132;" />
@@ -72,7 +71,7 @@ export const Navbar: FC = () => {
             <p className="text-xs font-semibold text-gray-dark lg:ml-2.5 lg:text-base">
               Alex
             </p>
-            <i className="bi bi-chevron-down ml-1 text-[10px] text-gray-dark lg:hidden" />
+            <ChevronDown className="ml-1 text-[10px] text-gray-dark lg:hidden" />
             {userOpen && (
               <HoverCard mobile={true} style={{ top: 30, right: 0 }}>
                 <CardItem link="logout" text="Sign Out &#10132;" />
--- apps/client/src/modules/navbar/components/Overlay.tsx
+++ apps/client/src/modules/navbar/components/Overlay.tsx
@@ -10,8 +10,9 @@ export const Overlay: FC<IOverlay> = ({ show }) => {
   return (
     <div
       className={
-        "z-95 flex h-[calc(100vh-48px)] items-center justify-center overflow-clip bg-[#F4F4F2] py-2" +
-        (show ? "opacity-100" : "h-0 w-0 opacity-0")
+        show
+          ? "z-95 flex h-[calc(100vh-48px)] items-center justify-center overflow-clip bg-[#F4F4F2] py-2 opacity-100 lg:hidden"
+          : "z-95 flex h-[calc(100vh-48px)] h-0 w-0 items-center justify-center overflow-clip bg-[#F4F4F2] py-2 opacity-0 lg:hidden"
       }
     >
       <ul className="h-100 space-y-8 text-center text-gray-dark">
