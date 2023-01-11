98ce5f01590d3a233380a7bd93dcc8aec0ea328f
Idhibhat Pankam <marginpankam@gmail.com>
2023-01-11 23:42:26 +0700

    feat: custom button

--- packages/design/src/Button.tsx
+++ packages/design/src/Button.tsx
@@ -1,22 +1,25 @@
-import { FC, PropsWithChildren, memo } from "react";
-
-import clsx from "clsx";
+import { FC, PropsWithChildren } from "react";
 
 interface ButtonProps extends PropsWithChildren {
   onClick?: () => void;
+  textColor: string;
+  backgroundColor: string;
 }
 
-export const Button: FC<ButtonProps> = memo(({ children, onClick }) => {
+export const Button: FC<ButtonProps> = ({
+  backgroundColor,
+  children,
+  onClick,
+  textColor,
+}) => {
   return (
     <button
-      className={clsx(
-        "rounded-lg bg-blue-600 p-4 text-xl font-bold text-white transition-colors hover:bg-blue-500"
-      )}
+      className={`rounded-full px-5 bg-${backgroundColor} py-3 text-sm font-bold text-${textColor} transition-colors hover:bg-black hover:text-white`}
       onClick={onClick}
     >
       {children}
     </button>
   );
-});
+};
 
 Button.displayName = "Button";
