b6837da3fb39451d1e4759d8d7ec4a1c8349e048
Idhibhat Pankam <marginpankam@gmail.com>
2023-01-12 15:22:12 +0700

    feat: login and register links in navbar

--- apps/client/src/modules/layouts/NavBar/NavBar.tsx
+++ apps/client/src/modules/layouts/NavBar/NavBar.tsx
@@ -1,6 +1,8 @@
 import { FC, useState } from "react";
 import { ChevronDown, FilterLeft, XLg } from "react-bootstrap-icons";
 
+import Link from "next/link";
+
 import { CardItem } from "./components/CardItem";
 import { DesktopItem } from "./components/DesktopItem";
 import { HoverCard } from "./components/HoverCard";
@@ -11,6 +13,7 @@ export const NavBar: FC = () => {
   const [navbarOpen, setNavbarOpen] = useState(false);
   const [gamesOpen, setGamesOpen] = useState(false);
   const [userOpen, setUserOpen] = useState(false);
+  const auth = false;
 
   return (
     <>
@@ -66,32 +69,82 @@ export const NavBar: FC = () => {
             </button>
           </DesktopItem>
         </div>
-        <div className="flex items-baseline">
-          <ProfilePic character="A" />
-          <button
-            className="relative ml-1 flex items-baseline hover:cursor-pointer lg:hover:cursor-default"
-            onClick={() => setUserOpen(!userOpen)}
-          >
-            <p className="text-xs font-semibold text-gray-dark lg:ml-2.5 lg:text-base">
-              Alex
-            </p>
-            <ChevronDown className="ml-1 text-[10px] text-gray-dark lg:hidden" />
-            {userOpen && (
-              <HoverCard mobile={true} style={{ top: 30, right: 0 }}>
-                <CardItem link="logout">
-                  <span>Sign Out </span>
-                  <span>&#10132;</span>
-                </CardItem>
-              </HoverCard>
-            )}
-          </button>
-          <button
-            className="ml-7 hidden text-base font-bold hover:cursor-pointer lg:block"
-            onClick={() => console.log("logout")}
-          >
-            <span>Sign Out &#10132;</span>
-          </button>
-        </div>
+        {auth ? (
+          <div className="flex items-baseline">
+            <ProfilePic character="A" />
+            <button
+              className="relative ml-1 flex items-baseline hover:cursor-pointer lg:hover:cursor-default"
+              onClick={() => setUserOpen(!userOpen)}
+            >
+              <p className="text-xs font-semibold text-gray-dark lg:ml-2.5 lg:text-base">
+                Alex
+              </p>
+              <ChevronDown className="ml-1 text-[10px] text-gray-dark lg:hidden" />
+              {userOpen && (
+                <HoverCard mobile={true} style={{ top: 30, right: 0 }}>
+                  <CardItem link="logout">
+                    <span>Sign Out </span>
+                    <span>&#10132;</span>
+                  </CardItem>
+                </HoverCard>
+              )}
+            </button>
+            <button className="ml-7 hidden text-base font-bold hover:cursor-pointer lg:block">
+              <Link
+                className="hover:text-cyan-dark hover:underline"
+                href="/logout"
+              >
+                Sign Out &#10132;
+              </Link>
+            </button>
+          </div>
+        ) : (
+          <div className="flex items-baseline">
+            <button
+              className="relative ml-1 flex items-baseline hover:cursor-pointer lg:hover:cursor-default"
+              onClick={() => setUserOpen(!userOpen)}
+            >
+              <p className="text-xs font-semibold text-gray-dark lg:ml-2.5 lg:hidden">
+                Sign In
+              </p>
+              <ChevronDown className="ml-1 text-[10px] text-gray-dark lg:hidden" />
+              {userOpen && (
+                <HoverCard mobile={true} style={{ top: 30, right: 0 }}>
+                  <CardItem link="login">
+                    <span>Login </span>
+                    <span>&#10132;</span>
+                  </CardItem>
+                  <CardItem link="register">
+                    <span>Register </span>
+                    <span>&#10132;</span>
+                  </CardItem>
+                </HoverCard>
+              )}
+            </button>
+            <button
+              className="ml-7 hidden text-base font-bold hover:cursor-pointer lg:block"
+              onClick={() => console.log("login")}
+            >
+              <Link
+                className="hover:text-cyan-dark hover:underline"
+                href="/login"
+              >
+                Login &#10132;
+              </Link>
+            </button>
+            <button
+              className="ml-7 hidden text-base font-bold hover:cursor-pointer lg:block"
+              onClick={() => console.log("register")}
+            >
+              <Link
+                className="hover:text-cyan-dark hover:underline"
+                href="/register"
+              >
+                Register &#10132;
+              </Link>
+            </button>
+          </div>
+        )}
       </div>
       <Overlay show={navbarOpen} />
     </>
