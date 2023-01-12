36e703423ee7ba03cc830d3c4de31ed40b230c5a
PongDev <pongwiwat.dev@gmail.com>
2023-01-12 11:52:28 +0700

    feat: implement google oauth

--- apps/server/src/auth/auth.service.ts
+++ apps/server/src/auth/auth.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+
+import { JWTPayload } from "@kitakita/types";
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private jwtService: JwtService) {}
+
+  async login(user: JWTPayload) {
+    const payload: JWTPayload = {
+      sub: user.username,
+      username: user.username,
+      role: user.role,
+    };
+
+    return this.jwtService.sign(payload);
+  }
+}
--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,18 +1,11 @@
 import { NestFactory } from "@nestjs/core";
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from "@nestjs/platform-fastify";
 
 import { AppModule } from "./app.module";
 
 import { ConfigService } from "./config/config.service";
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter()
-  );
+  const app = await NestFactory.create(AppModule);
 
   const configService = app.get(ConfigService);
   app.enableCors();
