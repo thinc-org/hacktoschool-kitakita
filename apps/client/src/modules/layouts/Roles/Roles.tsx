65373acbdf5ea30965a1804a3986ff24fa2e918b
Idhibhat Pankam <marginpankam@gmail.com>
2023-01-12 23:29:22 +0700

    fix: too small font in roles page

--- apps/client/src/modules/layouts/Roles/Roles.tsx
+++ apps/client/src/modules/layouts/Roles/Roles.tsx
@@ -9,7 +9,7 @@ import { RoleCard } from "./components/RoleCard";
 export const Roles: FC = () => {
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center p-4 lg:p-10">
-      <h1 className="font-sans text-4xl">Select a role</h1>
+      <h1 className="font-sans text-4xl lg:text-5xl">Select a role</h1>
       <div className="mt-8 flex h-4/5 w-screen flex-col items-center justify-around lg:flex-row">
         <RoleCard name="Student">
           <Image
