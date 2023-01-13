7b15da984acdc0e162c98a316530b49f217d795f
PongDev <70968612+PongDev@users.noreply.github.com>
2023-01-13 22:32:10 +0700

    feat: fix apollo graphql (#37)
    * chore(backend): install package for jwt
    
    * feat(server): implement jwt auth
    
    * fix: open apollo graphql

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 import { AppModule } from "./app.module";
 
@@ -9,6 +10,14 @@ async function bootstrap() {
 
   const configService = app.get(ConfigService);
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle("Kitakita")
+    .setDescription("KitaKita API description")
+    .setVersion("1.0")
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
 
   await app.listen(configService.port, configService.host);
   console.log(
